2f3b801421e936a52a6d48185fa1a3121904a92c
JEEVANKUMAR R <jeevan@JEEVANKUMARs-MacBook-Air.local>
2025-05-24 01:24:36 +0530

    added speed-insights package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer"
 import ProudSection from "@/components/proud"
 import FeaturesSection from "@/components/features"
 import BlogSection from "@/components/blog"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <BlogSection/>
       <ProudSection/>
       <Footer/>
+      <SpeedInsights/>
     </main>
   )
 }
